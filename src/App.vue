caef6902fd1ccf1cd6fdf855229ce8e054964b17
zhenganxia <769461729@qq.com>
2018-07-13 22:46:30 +0800

    多页标签实现切换

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
- <div id="app">
- <child1
- :p-child1="child1"
- :p-child2="child2"
- v-on:test1="onTest1" 
- v-on:test2="onTest2"> <!-- 此处监听了两个事件，可以在B组件或者C组件中直接触发 -->
- </child1>
- </div>
+
 </template>
 <script>
- import Child1 from './components/Child1.vue';
  export default {
  data () {
  return {
-  child1:'我是被子组件继承的内容',
-  child2:'我是被子组件的子组件继承的内容'
+  
  }
- },
- components: { Child1 },
- methods: {
- onTest1 () {
- console.log('test1 running...');
- },
- onTest2 () {
- console.log('test2 running');
  }
  }
  };
